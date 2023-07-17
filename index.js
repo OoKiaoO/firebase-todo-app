2c4f17f5a81f7f52d5de444a7e0041dec2d74ca4
Chiara Campalani <campalanichiara@gmail.com>
2023-07-17 17:35:47 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const addButton = document.getElementById("add-button");
+const cartInput = document.getElementById("input-field");
+
+addButton.addEventListener("click", (event) => {
+  let inputValue = cartInput.value;
+  console.log(inputValue);
+  });
